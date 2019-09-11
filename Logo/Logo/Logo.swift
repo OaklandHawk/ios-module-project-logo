03ff8af8554dce42e8858c7ff134fe4d6affc393
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-11 14:27:18 -0700

    Commented put unnecessary code

--- Logo/Logo/Logo.swift
+++ Logo/Logo/Logo.swift
@@ -22,11 +22,11 @@ class Logo: UIView {
 	private var line2 = Lines(width: 3.0, length: 3.0, color: .red, value: 0)
 	private var line3 = Lines(width: 3.0, length: 3.0, color: .blue, value: 0)
 	
-	private var lineHandEndPoint: CGPoint {
-		let line1AsRadians = Float(Double(line1.value) / 60 * 2.0 * Double.pi - Double.pi / 2)
-		let lineLength = CGFloat(frame.size.width / line1.length)
-		return handEndPoint(with: line1AsRadians, and: lineLength)
-	}
+//	private var lineHandEndPoint: CGPoint {
+//		let line1AsRadians = Float(Double(line1.value) / 60 * 2.0 * Double.pi - Double.pi / 2)
+//		let lineLength = CGFloat(frame.size.width / line1.length)
+//		return handEndPoint(with: line1AsRadians, and: lineLength)
+//	}
 	
 	private let bgColor = UIColor.yellow
 	private let borderColor = UIColor.green
@@ -163,10 +163,12 @@ class Logo: UIView {
 		}
 		
     }
+	
+	
 
-	private func handEndPoint(with radianValue: Float, and handLength: CGFloat) -> CGPoint {
-		return CGPoint(x: handLength * CGFloat(cosf(radianValue)) + frame.size.width / 2.0,
-					   y: handLength * CGFloat(sinf(radianValue)) + frame.size.height / 2.0)
-	}
+//	private func handEndPoint(with radianValue: Float, and handLength: CGFloat) -> CGPoint {
+//		return CGPoint(x: handLength * CGFloat(cosf(radianValue)) + frame.size.width / 2.0,
+//					   y: handLength * CGFloat(sinf(radianValue)) + frame.size.height / 2.0)
+//	}
 
 }
